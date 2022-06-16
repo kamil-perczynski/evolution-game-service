6b8b38966e7bcebb394efaa2dc00e8f4b8e7d824
Kamil Perczyński <kamil.perczynski.95@gmail.com>
2022-06-16 11:49:56 +0200

    Init commit

---
+++ src/main/java/perczynski/kamil/evolution/gameservice/EvolutionRecruitmentGameServiceApplication.java
@@ -0,0 +1,13 @@
+package perczynski.kamil.evolution.gameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolutionRecruitmentGameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolutionRecruitmentGameServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/perczynski/kamil/evolution/gameservice/EvolutionRecruitmentGameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package perczynski.kamil.evolution.gameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolutionRecruitmentGameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
